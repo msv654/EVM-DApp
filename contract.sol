20eb0131278265cdb5382f99c77ccf659f80753d
msv-654 <654mstv@gmail.com>
2024-03-16 20:24:59 +0100

    ownership transfering

--- contract.sol
+++ contract.sol
@@ -1,2 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+contract contractExample {
+
+string private text;
+
+
+constructor () {
+    text = "text example";
+}
+
+function functionExample() public view returns (string memory){
+    return text;
+}
+
+function setText(string memory newText) public{
+    text = newText;
+}
+
+}
