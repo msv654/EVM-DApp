f18e08a6ce09c7ed0610b39c091a116a86fcd6aa
msv-654 <654mstv@gmail.com>
2024-03-18 21:10:07 +0100

    hardhat install

--- contract.sol
+++ contract.sol
@@ -1,21 +1,31 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+    // SPDX-License-Identifier: MIT
+    pragma solidity ^0.8.0;
 
 contract contractExample {
+    string private text;
+    address public owner;
 
-string private text;
 
+    constructor() {
+        text = "text example";
+        owner = msg.sender;
+    }
 
-constructor () {
-    text = "text example";
-}
+    function funExample() public view returns (string memory) {
+        return text;
+    }
 
-function functionExample() public view returns (string memory){
-    return text;
-}
+    function setText(string calldata newText) public onlyOwner {
+        text = newText;
+    }
 
-function setText(string memory newText) public{
-    text = newText;
-}
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
 
+    modifier onlyOwner()
+    {
+        require (msg.sender == owner, "Caller is not the owner");
+        _;
+    }
 }
