cb76302335f3374643a3ffc7f5c9d35d78708404
msv-654 <654mstv@gmail.com>
2024-03-31 17:12:35 +0200

    3rd week project report

---
+++ project-proofs.ts
@@ -0,0 +1,13 @@
+/* 
+
+1) Contract Creation - 
+https://sepolia.etherscan.io/tx/0xcecf497a3e17512c27b03e7f725fa61e699b4ca34aa9f0465858871b52914822
+
+2) Token mint - https://sepolia.etherscan.io/tx/0xc4f7aed8c1afbff8834ec705e0845a8c38b279c7f0b7548897268b676eee85b6
+
+3) Self delegation - https://sepolia.etherscan.io/tx/0xa28a7964cc9fd0d59955434669714b67f0abd9e9fd047a127bba7bbd2e2a96a3
+
+4,5,6) Transfer tokens and check voiting power before and after
+https://sepolia.etherscan.io/tx/0x1bca59203b03c6596d6a75fd0ce97438c2e3683e30bb5497573996f1f24a780c 
+
+*/
